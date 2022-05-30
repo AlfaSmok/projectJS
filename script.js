cbaae77759dcc8dcbffc4f8bfaece2c76b7c30b1
Alfir Zagidullin <alfir1991@gmail.com>
2022-05-31 00:53:36 +0300

    Выполнено второе задание

---
+++ script.js
@@ -0,0 +1,18 @@
+const title = 'Course JS';
+const screens = 'Простые, Сложные, Интерактивные';
+const screenPrice = 2569;
+const rollback = 35;
+const fullPrice = 60000;
+const adaptive = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов " + screenPrice + " рублей \nСтоимость разработки сайта " + fullPrice + " рублей");
+
+console.log(screens.toLowerCase().split(", "));
+
+console.log("Процент отката посреднику за работу " +(fullPrice * (rollback/100)));
