d3b787c3a89f1e51d9536eedaf21b342789e2246
Alfir Zagidullin <alfir1991@gmail.com>
2022-06-02 20:54:42 +0300

    исправлены замечания по 3 заданию

--- script.js
+++ script.js
@@ -1,32 +1,25 @@
 'use strict';
 
 const title = prompt('Как называется ваш проект?');
-console.log(title);
 const screens = prompt('Какие типы экранов нужно разработать?');
-console.log(screens);
 const screenPrice = +prompt('Сколько будет стоить данная работа?');
-console.log(screenPrice);
-const adaptive = prompt('Нужен ли адаптив на сайте?');
-console.log(!!adaptive);
+const adaptive = confirm('Нужен ли адаптив на сайте?');
 const service1 = prompt('Какой дополнительный тип услуги нужен?');
-console.log(service1);
 const servicePrice1 = +prompt('Сколько это будет стоить?');
-console.log(servicePrice1);
 const service2 = prompt('Какой дополнительный тип услуги нужен?');
-console.log(service2);
 const servicePrice2 = +prompt('Сколько это будет стоить?');
-console.log(servicePrice2);
 const fullPrice = screenPrice + servicePrice1 + servicePrice2;
-console.log(fullPrice);
-
-
-
 const rollback = 35;
 
-
-
-
-
+console.log(title);
+console.log(screens);
+console.log(screenPrice);
+console.log(!!adaptive);
+console.log(service1);
+console.log(servicePrice1);
+console.log(service2);
+console.log(servicePrice2);
+console.log(fullPrice);
 
 console.log(typeof title);
 console.log(typeof fullPrice);
@@ -47,10 +40,10 @@ switch (true) {
   case fullPrice >= 30000:
     console.log("Даем скидку в 10%");
     break;
-  case 15000 <= fullPrice && fullPrice < 30000:
+  case 15000 <= fullPrice:
     console.log("Даем скидку в 5%");
     break;
-  case 0 < fullPrice && fullPrice < 15000:
+  case 0 < fullPrice:
     console.log("Скидка не предусмотрена");
     break;
   default:
