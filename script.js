69710dbc74290ec89f1c5aca2c12c7ece6738307
Alfir Zagidullin <alfir1991@gmail.com>
2022-06-12 23:07:44 +0300

    Выполнено 5 задание

---
+++ script.js
@@ -0,0 +1,116 @@
+'use strict';
+
+let title;
+let screens;
+let screenPrice;
+let adaptive;
+const rollback = 35;
+
+let allServicePrice;
+let fullPrice;
+let servicePercentPrice;
+let service1;
+let service2;
+
+const isNumber = function(num) {
+  return !isNaN(parseFloat(num)) && isFinite(num);
+};
+
+const asking = function() {
+  title = prompt('Как называется ваш проект?');
+  screens = prompt('Какие типы экранов нужно разработать?');
+
+  /* screenPrice = prompt('Сколько будет стоить данная работа?'); */
+  do {
+    screenPrice = prompt('Сколько будет стоить данная работа?');
+  } while (!isNumber(screenPrice));
+  
+  
+  adaptive = confirm('Нужен ли адаптив на сайте?');
+};
+
+const getAllServicePrice = function() {
+  let sum = 0;
+  for (let i = 0; i < 2; i++) {
+    if (i === 0) {
+      service1 = prompt('Какой дополнительный тип услуги нужен?');
+    } else if (i === 1) {
+      service2 = prompt('Какой дополнительный тип услуги нужен?');
+    }
+
+    do {
+      sum = prompt('Сколько это будет стоить?');
+    } while (!isNumber(sum));
+    
+
+  }
+
+  return sum;
+};
+
+function getFullPrice() {
+  fullPrice = screenPrice + allServicePrice;
+  return fullPrice;
+}
+
+const getTitle = function(str) {
+  str = str.trim();
+  return str[0].toUpperCase() + str.slice(1);
+};
+
+const getServicePercentPrices = function() {
+  return Math.ceil(fullPrice - (fullPrice * (rollback/100)));
+};
+
+const showTypeOf = function(variable) {
+  console.log(variable, typeof variable);
+};
+
+const getRollbackMessage = function(price) {
+  switch (true) {
+    case price >= 30000:
+      return "Даем скидку в 10%";
+    case 15000 <= price:
+      return "Даем скидку в 5%";
+    case 0 < price:
+      return "Скидка не предусмотрена";
+    default:
+      return "Что то пошло не так";
+  }
+};
+
+
+asking();
+allServicePrice = getAllServicePrice();
+fullPrice = getFullPrice();
+title = getTitle(title);
+servicePercentPrice = getServicePercentPrices();
+
+
+console.log(title);
+console.log(screens);
+console.log(screenPrice);
+console.log(!!adaptive);
+console.log(service1);
+console.log(service2);
+console.log(fullPrice);
+console.log(allServicePrice);
+
+showTypeOf(title);
+showTypeOf(fullPrice);
+showTypeOf(adaptive);
+
+
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+console.log(screens.toLowerCase().split(", "));
+
+console.log("Процент отката посреднику за работу " + (fullPrice * (rollback/100)));
+console.log(servicePercentPrice);
+console.log(getRollbackMessage(fullPrice));
+
+
+
+
